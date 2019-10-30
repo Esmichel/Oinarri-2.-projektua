11bfc3696b196685b008ec9422983cf9925b8245
sergio <svequiza@gmail.com>
2019-10-30 07:52:42 +0100

    ateekin

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-/var frame = window.requestAnimationFrame ||
+var frame = window.requestAnimationFrame ||
 			window.mozRequestAnimationFrame ||
 			window.webkitRequestAnimationFrame ||
 			window.msRequestAnimationFrame;
@@ -7,8 +7,8 @@
 var Xposizioa =0;
 var numero = 0;
 var sprite =new Image();
-sprite.src= "personaje2.png";
-var canvas= document.querySelector ("#lienzo");
+sprite.src= "personaje2editado.png";
+var canvas= document.querySelector ("#coinAnimation");
 var ctx= canvas.getContext("2d");
 console.log("ctx",ctx);
 
@@ -18,19 +18,18 @@ console.log("ctx",ctx);
 
 function tiempo(){
 
-if (numero >= 888){numero = 0}else{numero+=24}
+if (numero >= 637){numero = 0}else{numero+=24}
 
-for(var i = 0; i <= numero; i+=111){
+for(var i = 0; i <= numero; i+=80){
 
 	if(numero >= i){Xposizioa = i}
 }
 
-ctx.clearRect(0,0,canvas.width,canvas.height);
-ctx.drawImage(sprite,Xposizioa, 0, 110, 133, 0 , 280, 80, 100);
+ctx.clearRect(0,340,80,90);
+ctx.drawImage(sprite,Xposizioa, 0, 110, 133, 0 , 340, 80, 100);
 
 frame(tiempo)
 
 }
 
 tiempo()
-/
